57378a664cc8b2bbc984af9466ee13d033b9f77f
Ferdousfrd <ferdous.frd81@gmail.com>
2024-03-02 01:19:38 +0200

    started doing project 1 wiki

---
+++ encyclopedia/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+
+from . import util
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+        
+    })
+
+def entry(request, title):
+    entry_file = convert_md_to_html(title)
+    if entry_file == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/entry.html")
